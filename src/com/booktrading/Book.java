f6ef9e694eff614a9162378011c2181c8534ec31
Adriana <aeromeda@gmail.com>
2017-03-25 17:47:08 +0200

    Check it out Vix!

--- src/com/booktrading/Book.java
+++ src/com/booktrading/Book.java
@@ -1,8 +1,9 @@
 package com.booktrading;
 
+import java.io.Serializable;
 import java.util.UUID;
 
-public class Book {
+public class Book implements Serializable{
 
 	private String title;
 	private String author;
@@ -11,52 +12,70 @@ public class Book {
 	private boolean isForeignBook;
 	private int inStock;
 	private UUID id;
-	
-	
-	
+
+	public Book(String title, String author, double price, String publisher, boolean isForeignBook, int inStock) {
+		this.title = title;
+		this.author = author;
+		this.price = price;
+		this.publisher = publisher;
+		this.isForeignBook = isForeignBook;
+		this.inStock = inStock;
+	}
 
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
+
 	public String getPublisher() {
 		return publisher;
 	}
+
 	public void setPublisher(String publisher) {
 		this.publisher = publisher;
 	}
+
 	public boolean isForeignBook() {
 		return isForeignBook;
 	}
+
 	public void setForeignBook(boolean isForeignBook) {
 		this.isForeignBook = isForeignBook;
 	}
+
 	public int getInStock() {
 		return inStock;
 	}
+
 	public void setInStock(int inStock) {
 		this.inStock = inStock;
 	}
+
 	public UUID getId() {
 		return id;
 	}
+
 	public void setId(UUID id) {
 		this.id = id;
 	}
-	
-	
+
 }
--- src/com/booktrading/BookManager.java
+++ src/com/booktrading/BookManager.java
@@ -1,5 +1,6 @@
 package com.booktrading;
-import java.io.Serializable;
+
+import java.io.*;
 import java.util.*;
 
 public class BookManager {
@@ -12,11 +13,11 @@ public class BookManager {
 		return newBook.getId();
 	}
 
-	public List<Book> searchBook(String searchedBook) {
+	public List<Book> searchBook(String searchedTitle) {
 		List<Book> results = new ArrayList<>();
 		for (int i = 0; i < bookList.size(); i++) {
 			Book currentBook = bookList.get(i);
-			if (currentBook.getTitle().contains(searchedBook)) {
+			if (currentBook.getTitle().contains(searchedTitle)) {
 				results.add(currentBook);
 			}
 		}
@@ -48,19 +49,29 @@ public class BookManager {
 
 	}
 
-	
 	public boolean deleteBook(Book bookToRemove) {
 		return bookList.remove(bookToRemove);
 	}
-	
-	
-	
-	public void loadFromFile(){
-		
+
+	public boolean loadFromFile() throws IOException {
+		File source = new File("booklist");
+		if (!source.exists()) {
+			return false;
+		}
+		try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(source))) {
+			bookList = (List<Book>) in.readObject();
+		} catch (ClassNotFoundException | ClassCastException e) {
+			throw new IOException(e);
+		}
+		return true;
 	}
-	
-	public void saveToFile(){
-		
+
+	public void saveToFile() throws IOException {
+		try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("booklist"))) {
+
+			out.writeObject(bookList);
+		}
+
 	}
-	
+
 }
